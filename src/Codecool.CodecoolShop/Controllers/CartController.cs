46e4a41115140206d62472a1b0bf75b554ef086e
SergiuPert <sergiupert@gmail.com>
2022-05-04 10:17:18 +0300

    cart controller

---
+++ src/Codecool.CodecoolShop/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Codecool.CodecoolShop.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Cart()
+        {
+
+            return View();
+        }
+    }
+}
