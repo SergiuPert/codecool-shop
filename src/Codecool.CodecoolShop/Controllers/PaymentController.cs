d0fb65460ef24ecfb93a019be1b56881dd5aff2d
Alexandra Dumitru <alexandra.dumitru2811@gmail.com>
2022-05-05 16:47:14 +0300

    i have started making the checkout page/payment page

---
+++ src/Codecool.CodecoolShop/Controllers/PaymentController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Codecool.CodecoolShop.Daos;
+using Codecool.CodecoolShop.Daos.Implementations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Codecool.CodecoolShop.Models;
+using Codecool.CodecoolShop.Services;
+
+namespace Codecool.CodecoolShop.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+        public OrderService OrderService { get; set; }
+
+        public PaymentController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+            OrderService = new OrderService(
+                ProductDaoMemory.GetInstance(),
+                ProductCategoryDaoMemory.GetInstance(),
+                SupplierDaoMemory.GetInstance());
+        }
+
+        [Route("/")]
+        [Route("/Product")]
+        [Route("/Product/Index")]
+        [Route("/Product/Index/{id}")]
+
+        public IActionResult Index(int id = 1)
+        {
+            var products = ProductService.GetProductsForCategory(id);
+            return View(products.ToList());
+        }
+        public IActionResult SupplierSort(int id = 1)
+        {
+            var products = ProductService.GetProductsForSupplier(id);
+            return View(products.ToList());
+        }
+
+
+        //Add to cart route
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
