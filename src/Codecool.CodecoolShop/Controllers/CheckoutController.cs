32f6270eca44cd74c98a38bfd5def1a792705c84
Alexandra Dumitru <alexandra.dumitru2811@gmail.com>
2022-05-19 16:00:03 +0300

    addded checkout page and checkout validation

--- src/Codecool.CodecoolShop/Controllers/CheckoutController.cs
+++ src/Codecool.CodecoolShop/Controllers/CheckoutController.cs
@@ -13,28 +13,40 @@ using System.Text;
 using Microsoft.AspNetCore.Http;
 
 namespace Codecool.CodecoolShop.Controllers {
-    public class PaymentController : Controller {
-        private readonly ILogger<PaymentController> _logger;
+    public class CheckoutController : Controller {
+
+        private readonly ILogger<CheckoutController> _logger;
         private OrderService OrderService { get; set; }
 
-        public PaymentController(ILogger<PaymentController> logger,OrderService orderService) {
+        public CheckoutController(ILogger<CheckoutController> logger,OrderService orderService) 
+        {
             _logger = logger;
             OrderService = orderService;
         }
 
-        [Route("/Payment/PaymentPage")]
+        [HttpGet]
+        [Route("/Checkout/CheckoutPage")]
         public IActionResult Index() {
             string userId = Encoding.ASCII.GetString(HttpContext.Session.Get("user"));
-            return View("PaymentPage",
-                new PaymentModel() { Order = OrderService.GetOrder(userId)});
+            return View("CheckoutPage",
+                new Checkout() { Order = OrderService.GetOrder(userId)});
         }
 
+        
         [HttpPost]
-        [Route("/Payment/PaymentComplete")]
-        public IActionResult ProcessPayment(PaymentModel paymentModel) {
-            string name = paymentModel.FullName; // <-- Aleca, ce ai vrut sa faci cu asta?
-            return View("PaymentComplete");
+        [Route("/Checkout/CheckoutPage")]
+        public IActionResult ValidationCheckout(Checkout model)
+        {
+            string userId = Encoding.ASCII.GetString(HttpContext.Session.Get("user"));
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Payment");
+            }
+            else return View("CheckoutPage",
+                new Checkout() { Order = OrderService.GetOrder(userId) });
         }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
